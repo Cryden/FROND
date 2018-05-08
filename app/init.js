2b4d7bd1033adc5d8cea54a60c36143e7f31a0f9
CRYDEsigN <Cryden_da@mail.ru>
2018-05-08 11:50:32 +0300

    add config app

--- app/init.js
+++ app/init.js
@@ -1,5 +1,10 @@
 const fs = require('fs')
 
+const express = require('express')
+const app = express()
+
+const port = 8000
+
 function checkDefaultConfig () {
   if (fs.existsSync('frond.config.yml')) {
   } else {
@@ -10,4 +15,22 @@ function checkDefaultConfig () {
   }
 }
 
-module.exports = checkDefaultConfig()
+function init () {
+  app.get('/', (request, response) => {
+    response.send('Configure FROND!')
+  })
+
+  app.post('/', (request, response) => {
+    checkDefaultConfig()
+    response.send('FROND config file created!')
+  })
+
+  app.listen(port, (err) => {
+    if (err) {
+      return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
+  })
+}
+
+module.exports = init()
